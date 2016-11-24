28af6ebd6d3b48fca652ebf2cd553170d08052d0
adamwinwho <hs15952016403@gmail.com>
2016-11-24 22:40:34 +0800

    增加文章列表显示功能

--- ttbug/articles/views.py
+++ ttbug/articles/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from blocks.views import Blocks
+from .models import Articles
 
 # Create your views here.
 
 def articles_list(request,block_id):
     block_id = int(block_id)
-    return render(request,"articles_list.html")
+    blocks = Blocks.objects.all()
+    block = Blocks.objects.get(id=block_id)
+    articles = Articles.objects.filter(block=block).order_by("-id")
+    return render(request,"articles_list.html",{"blocks":blocks,"articles":articles})
 
 def article_detail(request,article_id):
     article_id = int(article_id)
--- ttbug/blocks/views.py
+++ ttbug/blocks/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import Blocks
 # Create your views here.
 from django.http import HttpResponse
+from articles.models import Articles
 
 def index(request):
     blocks = Blocks.objects.all().order_by("id")
-    return render(request,"index.html",{"blocks":blocks})
+    articles = Articles.objects.all().order_by("-id")
+    return render(request,"index.html",{"blocks":blocks,"articles":articles})
