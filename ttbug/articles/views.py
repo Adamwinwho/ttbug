91562d3f8acb4d00d1376d3e1e74bc0e2f6f3281
adamwinwho <hs15952016403@gmail.com>
2016-11-25 23:07:21 +0800

    增加发表文章功能

--- ttbug/articles/views.py
+++ ttbug/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from blocks.views import Blocks
 from .models import Articles
 
@@ -9,12 +9,26 @@ def articles_list(request,block_id):
     blocks = Blocks.objects.all()
     block = Blocks.objects.get(id=block_id)
     articles = Articles.objects.filter(block=block).order_by("-id")
-    return render(request,"articles_list.html",{"blocks":blocks,"articles":articles})
+    return render(request,"articles_list.html",{"block_id":block_id,"blocks":blocks,"articles":articles})
 
 def article_detail(request,article_id):
     article_id = int(article_id)
-    pass
+    article = Articles.objects.get(id=article_id)
+    blocks = Blocks.objects.all()
+
+    return render(request,"article_detail.html",{"block_id":article.block_id,"blocks":blocks,"article":article})
 
 def article_create(request,block_id):
     block_id = int(block_id)
-    pass
+    blocks = Blocks.objects.all()
+    if request.method == "GET":
+        return render(request,"article_create.html",{"blocks":blocks,"block_id":block_id})
+    else:
+        title = request.POST["title"]
+        content = request.POST["content"]
+        block = Blocks.objects.get(id=block_id)
+        article = Articles(block=block,title=title,content=content)
+        article.save()
+
+        return redirect("/articles/list/%s" % block_id,{"blocks":blocks})
+    
