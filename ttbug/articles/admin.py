29a431f99b0b113b846516ae1254b5a10290f892
adamwinwho <hs15952016403@gmail.com>
2016-11-27 16:08:04 +0800

    增加后台文章markdown编辑支持

--- ttbug/articles/admin.py
+++ ttbug/articles/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Articles
+from pagedown.widgets import AdminPagedownWidget
+from django import forms
 # Register your models here.
+
+class ArticleForm(forms.ModelForm):
+    content = forms.CharField(widget=AdminPagedownWidget())
+
+    class Meta:
+        model = Articles
+        fields = "__all__"
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ("title","author","last_update_timestamp")
+    form = ArticleForm
 
 admin.site.register(Articles,ArticleAdmin)
+
